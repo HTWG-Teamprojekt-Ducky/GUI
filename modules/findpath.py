40f78825320f8f234d0725d3531eabad99d8c43f
Animesh <anim_esh@yahoo.co.in>
2020-09-07 20:36:43 +0200

    ++ refractored file and added ducky animation ++

--- modules/findpath.py
+++ modules/findpath.py
@@ -1,16 +1,16 @@
 class Findpath():
-    start_set = False;
+    start_set = False
     end_set = False
-    start = [];
-    end = [];
+    start = []
+    end = []
     ovals = []
 
     def __init__(self, canvas):
         self.canvas = canvas
 
     def set_start(self, event):
-        global start_set;
-        global start_point;
+        global start_set
+        global start_point
         global start
 
         if start_set:
@@ -18,9 +18,9 @@ class Findpath():
 
         self.canvas.create_rectangle(event.x, event.y, event.x + 6, event.y + 6, fill="green", tags="start")
         start_set = True
-        x = int(round(event.x / 12));
+        x = int(round(event.x / 12))
         y = int(round(event.y / 12))
-        start.append(x);
+        start.append(x)
         start.append(y)
 
         start_point.set('{}, {}'.format(x, y))
